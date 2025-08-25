a80728450d322d2649e267b539d48bca77ee0f3b
PedroFreitasM <130061255+PedroFreitasM@users.noreply.github.com>
2025-08-25 16:31:24 -0300

    Add files via upload

---
+++ lab3_prod_int.c
@@ -0,0 +1,102 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include "timer.h"
+
+//variável para o produto interno
+float *vet1;
+float *vet2;
+
+//estrutura de dados para os argumentos das threads
+typedef struct{
+	long int tam;
+	short int nthreads;
+	short int id;
+} t_args;
+
+void *prodVetor (void *args){
+	t_args *arg = (t_args *) args;
+	int ini, fim, fatia;
+	float prod_local=0; *ret;
+	
+	fatia = arg->tam / arg->nthreads;
+	ini = arg->id * fatia;
+	fim = ini + fatia;
+	if (arg->id == (arg->nthreads-1)) fim = arg->tam; // ultima thread pega os elementos finais 
+	
+	//faz o produto
+	for(int i = ini; i<fim; i++){
+		prod_local += (vet1[i]*vet2[i]);
+	}
+
+	ret = (float *) malloc(sizeof(float));
+	if (ret!=NULL) *ret = prod_local;
+	else printf("ERRO no malloc da thread\n");
+	pthread_exit((void *) ret);
+}
+
+//funcao main
+int main(int argc, char *argv[]){
+	long int n; //tam do vetor
+	short int nthreads; //qt de threads
+	FILE *arq; //arquivo de entrada
+	size_t ret;
+	double prod_ori;
+	float prod_par_global;
+	float *prod_retorno_threads;
+
+	pthread_t *tid_sistema;
+
+	if(argc <3) {
+	printf("Uso: %s <arquivo de entrada> <qt de threads> \n", argv[0]);
+	exit(-1);
+	}
+
+	//abre o arquivo
+	arq = fopen(argv[1], "rb");
+	if(arq==NULL) { 
+		printf("ERRO na abertura de arquivo\n");
+		exit(-1);
+	}
+
+	ret = fread(&n, sizeof(long int), 1, arq);
+	if(!ret){
+		fprintf(stderr, "Erro de leitura da dimensao das matrizes \n");
+		return 3;
+	}
+
+	vet1 = (float *)malloc(sizeof(float) * n);
+	vet2 = (float *)malloc(sizeof(float) * n);
+	if(vet1 == NULL || vet2 == NULL){
+		printf("ERRO no malloc dos vetores\n");
+		exit(-1);vet
+	}
+	ret = fread(vet1, sizeof(float), n, arq);
+	ret = fread(vet2, sizeof(float), n, arq);
+
+	//le o numero de threads
+	nthreads = atoi(argv[2]);
+
+	//se o vetor for muito pequeno, limita no num de threads
+	if(nthreads>n) nthreads = n;
+
+	tid_sistema = (pthread_t *)malloc(sizeof(pthread_t) * nthreads);
+	if(tid_sistema==NULL){
+		printf("ERRO no malloc das IDs das threads\n");
+		exit(-1);
+	}
+
+	//cria threads
+	for(long int i = 0; i < nthreads; i++){
+		t_args *args;
+		args = (t_args*) malloc(sizeof(t_args));
+		if(args==NULL){
+			printf("ERRO no malloc dos argumentos\n");
+			exit(-1);
+		}
+		args->tam = n;
+		args->nthreads = nthreads;
+		args->id = i;
+		if (pthread_create)
+	}
+}
