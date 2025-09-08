07059e3fa99f8b4e81ef8b02403ec963f2eb5fe5
PedroFreitasM <130061255+PedroFreitasM@users.noreply.github.com>
2025-09-08 19:00:11 -0300

    Add files via upload

---
+++ ex2.c
@@ -0,0 +1,81 @@
+//Aluno: Pedro Freitas de Moura
+/* Objetivo do código: Alterar o código soma-lock-atom.c para imprimir os valores múltiplos de 1000 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#define LIMITE 10000
+
+long int soma = 0; //var de soma
+pthread_mutex_t mutex;// var de lock para exclusao mutua
+pthread_cond_t cond;
+
+void *extra (void *args){
+	printf("Inicio da func. extra\n");
+	for (int i = 0; i < 100000; i++){
+		pthread_mutex_lock(&mutex);
+		while ((soma%1000)){
+			pthread_cond_wait(&cond, &mutex);
+		}
+		if (soma < LIMITE){
+			printf("Soma = %ld \n", soma);
+		}
+		pthread_mutex_unlock(&mutex);
+	}
+	printf("Fim da func. extra \n");
+	pthread_exit(NULL);
+}
+
+void *ExecutaTarefa(void *arg){
+	long int id = (long int) arg;
+	printf("Thread %ld sendo executada \n", id);
+
+	for (int i = 0; i < 100000; i++){
+		pthread_mutex_lock(&mutex);
+		soma++;
+		if(!(soma%1000)){
+			pthread_cond_signal(&cond);
+		}
+		pthread_mutex_unlock(&mutex);
+	}
+	printf("Fim da thread %ld \n" , id);
+	pthread_exit(NULL);
+}
+
+int main(int argc, char *argv[]){
+	pthread_t *tid;
+	int nthreads;
+	
+	if(argc<2){
+		printf("Comando: %s <num de threads>\n", argv[0]);
+		return 1;
+	}
+	nthreads = atoi(argv[1]);
+
+	tid = (pthread_t*) malloc(sizeof(pthread_t)*(nthreads+1));
+	if(tid==NULL) {puts("Erro no malloc"); return 2;}
+
+	pthread_mutex_init(&mutex, NULL);
+	pthread_cond_init(&cond, NULL);
+
+	for(long int t = 0; t<nthreads; t++){
+		if (pthread_create(&tid[t], NULL, ExecutaTarefa, (void *) t)){
+			printf("Erro no pthread_create\n"); exit(-1);
+		}
+	}
+
+	if (pthread_create(&tid[nthreads], NULL, extra, NULL)){
+		printf("Erro no pthread_create do log\n"); exit(-1);
+	}
+
+	for (int t=0; t<nthreads+1; t++){
+		if(pthread_join(tid[t], NULL)){
+			printf("Erro no join das threads\n"); exit(-1);
+		}
+	}
+	pthread_mutex_destroy(&mutex);
+	pthread_cond_destroy(&cond);
+	printf("Valor da var. soma = %ld \n", soma);
+
+	return 0;
+}
