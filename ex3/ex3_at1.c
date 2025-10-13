cd773a44de3034c2c8d3a1dca706107e967333c4
Pedro Freitas <pedrofm@dcc.ufrj.br>
2025-10-13 18:44:52 -0300

    feats iniciais

--- ex3/ex3_at1.c
+++ ex3/ex3_at1.c
@@ -9,11 +9,28 @@
 
  A thread produtora insere todos os elementos do buffer de uma vez. As consumidoras retiram eles, um de cada vez, e avaliam se eles são primos.
 
+ Além disso, a sincronização das threads deve ser feita com semáforos.
+
  Input do programa: ./<nome do programa> <numero de threads consumidoras> <valor de m> <valor de n>
  */
+sem_t slotCheio, slotVazio; //condição
+sem_t mutexCons; //mutex
+			    //
+int contador=0, insere=0, remove=0;
+int* buffer;
 
+// funcao do unico produtor
+void* produz (void* args){
+	int* n = (int *)args;
+	for (int i = 0; i <= n; i++){
+			
+	}
+}
+
+// funcao dos consumidores
 void* ehPrimo(void* num){
-	long long int n = (long long int *) num
+
+	long long int* n = (long long int *) num
 	int i;
 	if (n <= 1) return 0;
 	if (n == 2) return 1;
@@ -21,4 +38,31 @@ void* ehPrimo(void* num){
 	for (i=3; i<sqrt(n)+1; i+=2)
 		if(n%i==0) return 0;
 	return 1;
+}
+
+int main(int argc, char** argv){
+	pthread_t *tid;
+	int nthreads;
+	int m, n;
+	if(argc < 4){
+		printf("Comando: %s <num de threads consumidoras> <tamanho m do buffer> <quantidade de numeros (n)>\n", argv[0]);
+		return 1;
+	}
+	nthreads = atoi(argv[1]);
+	m = atoi(argv[2]);
+	n = atoi(argv[3]);
+	buffer = (int *)malloc(sizeof(int)*m);	
+	tid = (pthread*)malloc(sizeof(pthread_t)*(nthreads+1));
+	if(tid==NULL) {puts("Erro no malloc"); return 2;}
+	
+	sem_init(&mutexCons, 0, 1);
+	sem_init(&slotCheio, 0, 0);
+	sem_init(&slotVazio, 0, m);
+
+
+
+
+
+
+	return 0;
 }
