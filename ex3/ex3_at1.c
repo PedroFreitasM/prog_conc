a760d33d0d11959f5131a2eec4c91a104eca1826
PedroFreitasM <130061255+PedroFreitasM@users.noreply.github.com>
2025-10-13 09:58:19 -0300

    Add files via upload

---
+++ ex3/ex3_at1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+/*
+ Autor: Pedro Freitas de Moura
+ Matéria: Programação Concorrente
+ Objetivo do código: Criar uma thread produtora que gera uma sequência N de nums. inteiros consecutivos e os insere no canal de inteiros de tamanho M (M << N) que será utilzado por várias threads consumidoras.
+
+ A thread produtora insere todos os elementos do buffer de uma vez. As consumidoras retiram eles, um de cada vez, e avaliam se eles são primos.
+
+ Input do programa: ./<nome do programa> <numero de threads consumidoras> <valor de m> <valor de n>
+ */
+
+void* ehPrimo(void* num){
+	long long int n = (long long int *) num
+	int i;
+	if (n <= 1) return 0;
+	if (n == 2) return 1;
+	if (n%2 == 0) return 0;
+	for (i=3; i<sqrt(n)+1; i+=2)
+		if(n%i==0) return 0;
+	return 1;
+}
